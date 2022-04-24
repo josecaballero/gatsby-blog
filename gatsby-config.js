ef1d173f6c8d2e99b7ba080923ae6dd238642586
Jose <jlcab8@gmail.com>
2022-04-24 19:04:26 +0200

    change title blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `El Blog de Joseph`,
+    title: `El Blog de Jose`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
